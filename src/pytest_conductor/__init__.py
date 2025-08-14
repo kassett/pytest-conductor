eca1ce97eefb30b60146ca47c757928184b70af1
Samuel Chai <samuel.chai.development@gmail.com>
2025-08-14 14:00:38 -0400

    bumpign bversion

--- src/pytest_conductor/__init__.py
+++ src/pytest_conductor/__init__.py
@@ -1,6 +1,6 @@
 """Pytest plugin for coordinating the order in which marked tests run."""
 
-VERSION = "0.2.0"
+VERSION = "0.2.1"
 
 # Import hooks to register them with pytest
 from . import hooks  # noqa
